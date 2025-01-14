b58cd262a2e35287a56d82da030165aa765ebaea
uditgoyaludit <67572998+uditgoyaludit@users.noreply.github.com>
2025-01-14 10:18:05 +0530

    Add files via upload

---
+++ Admin/includes/sidebar.php
@@ -0,0 +1,74 @@
+ <ul class="navbar-nav sidebar sidebar-light accordion " id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center bg-gradient-primary  justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon" >
+          <img src="img/logo/attnlg.jpg">
+        </div>
+        <div class="sidebar-brand-text mx-3">AMS</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Class and Class Arms
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="
+          aria-expanded="true" aria-controls="collapseBootstrap">
+          <i class="fas fa-chalkboard"></i>
+          <span>Manage Classes</span>
+        </a>
+        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Classes</h6>
+            <a class="collapse-item" href="createClass.php">Create Class</a>
+            
+          </div>
+        </div>
+      </li>
+       <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Teachers
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="
+          aria-expanded="true" aria-controls="collapseBootstrapassests">
+          <i class="fas fa-chalkboard-teacher"></i>
+          <span>Manage Teachers</span>
+        </a>
+        <div id="collapseBootstrapassests" class="collapse" aria-labelledby="headingBootstrap" data-parent="
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Class Teachers</h6>
+             <a class="collapse-item" href="createClassTeacher.php">Create Class Teachers</a>
+              
+          </div>
+        </div>
+      </li>
+       
+
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Students
+      </div>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link collapsed" href="
+          aria-expanded="true" aria-controls="collapseBootstrap2">
+          <i class="fas fa-user-graduate"></i>
+          <span>Manage Students</span>
+        </a>
+        <div id="collapseBootstrap2" class="collapse" aria-labelledby="headingBootstrap" data-parent="
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Students</h6>
+            <a class="collapse-item" href="createStudents.php">Create Students</a>
+            
+          </div>
+        </div>
+      </li>
+      
+      <hr class="sidebar-divider">
+      
+    </ul>
